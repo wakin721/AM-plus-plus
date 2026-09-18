61bd3e3c8fef79a80f849809b0bddcbb9fcc3231
和錦わきん <156275155+wakin721@users.noreply.github.com>
2026-09-18 18:14:17 +0800

    test: align USB Direct expectations with handoff and buffer range

--- app/src/test/java/dev/amenhancer/module/config/OrdinarySettingsWritePolicyTest.kt
+++ app/src/test/java/dev/amenhancer/module/config/OrdinarySettingsWritePolicyTest.kt
@@ -100,7 +100,7 @@ class OrdinarySettingsWritePolicyTest {
                 "lyric_blur_radius_offset_px" to 6,
                 "usb_bit_perfect_enabled" to true,
                 "usb_direct_uac_enabled" to true,
-                "usb_direct_pcm_buffer_ms" to 500,
+                "usb_direct_pcm_buffer_ms" to 100,
                 "usb_direct_transfer_buffer_ms" to 0,
                 "title_correction_enabled" to false,
                 "title_correction_mode" to "original_hyper",
